16dbaba76363155975668800e97112e826d883ee
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2021-05-19 13:24:45 +0530

    fix : fixed snackbar bug

--- src/components/SnackBar.js
+++ src/components/SnackBar.js
@@ -1,17 +1,20 @@
 import React, { useEffect } from 'react'
+import { useData } from '../contexts/DataContext'
 import '../styles/snackbar.css'
 const Snackbar = ({message , type}) => {
+    const {dispatch} = useData()
      useEffect(() => {
         if(type === 'snackbar__primary'){
             let snackbarPrimary = document.querySelector('.snackbar__primary')
             snackbarPrimary.style.display = 'flex'
             setTimeout(() => snackbarPrimary.style.display = 'none' , 2500)
+            return () => dispatch({type : 'STATUS' , payload : null})
         }
         if(type === 'snackbar__secondary'){
             let snackbarPrimary = document.querySelector('.snackbar__secondary')
             snackbarPrimary.style.display = 'flex'
         }
-    },[type])
+    },[type , dispatch])
     return (
         <div className={`snackbar ${type}`}>
             <p>{message}</p>
